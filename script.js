285124731e82ff421ff9745b898657521423d547
Kirsti Esch <miss.k.esch@gmail.com>
2024-05-13 13:15:22 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// Create string with user message
+
+console.log("You have recieved this message because you have been chosen to open an important vault. Here is the secret combination:");
+
+// Assign variables to each number in the lock combo
+const firstNumber = (17 - 7)
+const secondNumber = (firstNumber * 4)
+const thirdNumber = (secondNumber - 1)
+
+//create dialog box displaying vault codes
+alert("The combination is: " + firstNumber + " - " + secondNumber + " - " + thirdNumber);
